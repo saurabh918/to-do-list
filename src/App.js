5cd68a17ce3172139403a779452edc7c1956e16e
saurabh-gaonkar <saurabhg@axioned.com>
2023-05-19 12:24:36 +0530

    Added: edit and filter functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,99 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Listitem from './components/Listitem'
 
-export default class App extends Component {
-  state = {
-    items : [],
-    text : ""
+const App = () => {
+
+  const [list,setList] = useState({items : [],text : ""})
+  const [edit,setEdit] = useState(false)
+  const[ editVal,setEditVal] = useState("")
+  const [editId,setEditId] = useState(null)
+  const [completed,setCompleted] = useState(false)
+  const [active,setActive] = useState(false)
+  console.log(list)
+  const handleChange = e => {
+    setList({...list,text : e.target.value})
   }
 
-  handleChange = e => {
-    this.setState({text : e.target.value})
-  }
-
-  handleAdd = (e) =>{
-    if(this.state.text !== "") {
-      const item = [...this.state.items , this.state.text];
-      this.setState({items : item , text : ""})
+  const handleAdd = (e) =>{
+    if(list.text !== "") {
+      if (edit) {
+        setList((prevState) => {
+          const updatedItems = prevState.items.map((item) => {
+            if (item.id === editId) {
+              return { ...item, value: list.text };
+            }
+            return item;
+          });
+          return { ...prevState, items: updatedItems, text: "" };
+        });
+        setEdit(false)
+       } else {
+      const item = {id: new Date().getTime().toString(),value: list.text,active: true,completed:false};
+      setList({...list,items: [...list.items,item],text: ""})
+      }
     }
   }
 
-  handleDelete = (id) => {
+
+
+  const handleDelete = (id) => {
     console.log(id)
-    const oldItems = [...this.state.items]
-    const newItems = oldItems.filter((item,i)=>{
-      return i !== id;
+    const oldItems = [...list.items]
+    const newItems = oldItems.filter((item)=>{
+      return item.id !== id;
     })
-    this.setState({items: newItems})
-    console.log(this.state.items)
+    setList({...list,items: newItems})
   }
 
-  render() {
+  const handleEdit = (value,id) => {
+    setEdit(true)
+    setEditVal(value)
+    setEditId(id)
+    setList({...list,text:value})
+    setList((prevState) => {
+      const updatedItems = list.items.map((item) => {
+        if(item.id === id) {
+          return {...item,completed: false}
+        }
+        return item
+      })
+      return {...prevState,items: updatedItems,text: value}
+    })
+  }
+
+  const handleStatus = (id) => {
+    setList((prevState) => {
+      const updatedItems = list.items.map((item) => {
+        if(item.id === id) {
+          return {...item,completed: true}
+        }
+        return item
+      })
+      return {...prevState,items: updatedItems,text: ""}
+    })
+    console.log(list)
+  }
+
+
+  const defaultArr = [];
+  defaultArr.push(...list.items); /* default data */
+  
+  if (completed) {
+    const filteredArr = defaultArr.filter(e => e.completed);
+    
+    defaultArr.length = 0; /* clear array for filtered data */
+    defaultArr.push(...filteredArr);
+  }
+
+  if (active) {
+    const filteredArr = defaultArr.filter(e => e.completed === false);
+    
+    defaultArr.length = 0; /* clear array for filtered data */
+    defaultArr.push(...filteredArr);
+  }
+
     return (
       <div className="container-fluid my-5">
         <div className="row ">
@@ -42,17 +105,22 @@ export default class App extends Component {
           <div className="col-5 mx-auto">
             <div className="row">
             <div className="col-9">
-            <input type="text" className='form-control' placeholder='Write here' value={this.state.text} onChange={this.handleChange} />
+            <input type="text" className='form-control' placeholder='Write here' value={list.text} onChange={handleChange} />
           </div>
           <div className="col-3 m-auto">
-            <button className='text-bg-warning pd-3 fw-bold' onClick={this.handleAdd}>Add</button>
+            <button className='text-bg-warning pd-3 fw-bold' onClick={handleAdd}>{edit ? "Edit" : "Add"}</button>
+          </div>
+          <div className="container-fluid">
+            <button className='bg-success text-white col-sm-3 m-2' onClick={()=>{setCompleted(true);setActive(false)}}>Show completed</button>
+            <button className='bg-warning text-black col-sm-3 m-2' onClick={()=>{setCompleted(false);setActive(true)}}>Show active</button>
+            <button className='bg-secondary text-white col-sm-3 m-2' onClick={()=>{setCompleted(false);setActive(false)}}>Show all</button>
           </div>
           <div className="container-fluid">
             <ul className="list-unstyled">
               {
-                this.state.items.map((value,i)=>{
-                     return <Listitem key={i} value={value} id={i} handleDelete={this.handleDelete}/>
-                }
+                defaultArr.map((item)=>{
+                     return <Listitem key={item.id} value={item.value} id={item.id} handleDelete={handleDelete} handleEdit={handleEdit} handleStatus={handleStatus} completed={item.completed} />
+                  }
 
                 )
               }
@@ -64,7 +132,6 @@ export default class App extends Component {
       </div>
       
     )
-  }
 }
 
-
+export default App;
--- src/components/Listitem.js
+++ src/components/Listitem.js
@@ -2,8 +2,11 @@ function Listitem (props) {
     return(
         <>
         <div className="row my-2 col-sm-9 d-flex align-items-center justify-content-between mx-2">
-        <li className="shadow p-2 my-2 col-sm-9">{props.value}</li>
-        <button className="bg-danger text-white col-sm-1 p-0 mx-4 h-50" onClick={()=>{props.handleDelete(props.id)}} >X</button>
+        <li className="shadow d-flex p-2 my-2 col-sm-6">{props.value}</li>
+        <button className="bg-dark text-white col-sm-1 p-0 h-50" onClick={()=>props.handleEdit(props.value,props.id)}>&#9998;</button>
+        <button className="bg-danger text-white col-sm-1 p-0 h-50" onClick={()=>{props.handleDelete(props.id)}} >X</button>
+        <button className="bg-success text-white col-sm-1 p-0 h-50" onClick={()=>{props.handleStatus(props.id)}}>{props.completed ? "\u2713" : "\u2610"} </button>
+        
         </div>
         </>
     )
